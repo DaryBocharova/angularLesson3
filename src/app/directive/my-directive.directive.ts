aa6c137d0b15c12243f39dfdfef5de3844675b0e
d.bocharova <d.bocharova@x5.ru>
2020-09-15 15:15:00 +0300

    исправление замечаний

--- src/app/directive/my-directive.directive.ts
+++ src/app/directive/my-directive.directive.ts
@@ -1,7 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[appMyDirective]'
+  selector: '[appMyDirective]',
 })
 export class MyDirectiveDirective {
+  private regex: RegExp;
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('click', ['$event'])
+  onClick(event) {
+    let current: string = this.el.nativeElement.value;
+    let next: string =
+      current.substring(0, this.el.nativeElement.selectionStart) +
+      event.key +
+      current.substring(this.el.nativeElement.selectionEnd);
+  }
 }
