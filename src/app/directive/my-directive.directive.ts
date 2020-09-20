81d2decca1449bf5ff0a59ae9002c0468d3f2a83
d.bocharova <d.bocharova@x5.ru>
2020-09-20 09:21:02 +0300

    lesson3

--- src/app/directive/my-directive.directive.ts
+++ src/app/directive/my-directive.directive.ts
@@ -4,17 +4,15 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appMyDirective]',
 })
 export class MyDirectiveDirective {
-  private regex: RegExp;
-
+  
   constructor(private el: ElementRef) {}
 
-  @HostListener('click', ['$event'])
-  onClick = (event) => this.action(event);
+  @HostListener('click') onClick = (event) => this.action(event);
 
-  action(event) {
+  action(event): void {
     let current: string = this.el.nativeElement.value;
     let element = current.search('_');
-    if (element >= 0) {
+    if (element > 0) {
       event.setSelectionRange(element, element);
     } else {
       event.setSelectionRange(element, current.length);
